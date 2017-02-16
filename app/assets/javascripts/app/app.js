498bf689e60b42d2d5c52e1eec112fd06c53de43
seohasong <tisutoo@gmail.com>
2017-02-16 19:01:55 +0900

    add carrierwave

--- app/assets/javascripts/app/app.js
+++ app/assets/javascripts/app/app.js
@@ -1,4 +1,19 @@
+function square(classname) {
+    function operator(objs){
+	   	$.each(objs, function(i, obj){
+	        obj=$(obj)
+	        child=$(obj.children()[0])
+	        width=parseInt(obj.css("width"))
+			obj.css("height", width)
+			obj.css("min-height", width)
+	        if(parseInt(child.css("height"))>width){child.css("height", "100%").removeClass("img") } 
+	    })
+	}
+    objs=$(classname)
+    $( document ).ready(function(){operator(objs)})
+	$( window ).resize(function(){operator(objs)})
+}
+
 function gogoTop(point) {
     $('body').animate({scrollTop: point}, '500');
 }
-
