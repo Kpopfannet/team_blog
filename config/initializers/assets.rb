360665a150f04bc05c24699e9c7df6277c929897
seohasong <tisutoo@gmail.com>
2017-02-23 13:46:15 +0900

    sync to YH: from SH

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,7 +12,9 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( _controller/members.css )
 Rails.application.config.assets.precompile += %w( _controller/blogs.css )
 Rails.application.config.assets.precompile += %w( _controller/home.css )
+Rails.application.config.assets.precompile += %w( _controller/articles.css )
 
 Rails.application.config.assets.precompile += %w( _controller/members.js )
 Rails.application.config.assets.precompile += %w( _controller/blogs.js )
 Rails.application.config.assets.precompile += %w( _controller/home.js )
+Rails.application.config.assets.precompile += %w( _controller/articles.js )
